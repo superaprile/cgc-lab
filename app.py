4b72265898a22b691763cfd55da276d3b56c8d00
superaprile <62966226+superaprile@users.noreply.github.com>
2022-06-04 17:13:12 +0200

    changed url for addons

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def apply_watermark():
     r1 = s3_client.upload_file(path, bucket_name, filename, ExtraArgs={'ACL': 'public-read'})
 
     # GENERATE REQUEST FOR QRACKAJACK
-    qr_req_url = ""
+    qr_req_url ='https://qrackajack.expeditedaddons.com/?api_key=' + os.environ['QRACKAJACK_API_KEY'] + '&bg_color=%23ffffff&content=http%3A%2F%2Fexample.org&fg_color=%23000000&height=256&width=256'
 
     qr_name = f"qr_{filename}"
     qr_path = request_and_save(qr_req_url, qr_name)
@@ -68,7 +68,7 @@ def apply_watermark():
 
 
     # GENERATE REQUEST FOR WATERMARKER
-    watermark_req_url = ""
+    watermark_req_url ='https://watermarker.expeditedaddons.com/?api_key=' + os.environ['WATERMARKER_API_KEY'] + '&height=100&image_url=https%3A%2F%2Fwww.expeditedaddons.com%2Fbase.png&opacity=50&position=center&watermark_url=https%3A%2F%2Fwww.expeditedaddons.com%2Fwatermark.png&width=100
 
     watermark_name = f"watermark_{filename}"
     request_and_save(watermark_req_url, watermark_name)
