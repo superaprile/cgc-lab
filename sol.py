81c76cf2c15be166123e5ed8287e218be8448f93
superaprile <62966226+superaprile@users.noreply.github.com>
2022-06-04 17:49:47 +0200

    Update sol.py

--- sol.py
+++ sol.py
@@ -52,7 +52,7 @@ def index():
 
 @app.route('/watermark', methods=['POST'])
 def apply_watermark():
-    bucket_name = "cgc-lab3"
+    bucket_name = "heroku1-bucket"
 
     filename = request.form['filename']
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
