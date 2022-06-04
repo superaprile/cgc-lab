d53494bfdd059bb4fe2d5385f40fa4c75dee9992
superaprile <62966226+superaprile@users.noreply.github.com>
2022-06-04 18:08:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def index():
 
 @app.route('/watermark', methods=['POST'])
 def apply_watermark():
-    bucket_name = "cgc-bucket"
+    bucket_name = "oltrebucket"
 
     filename = request.form['filename']
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
